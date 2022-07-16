cdce81a017c52c756f60142c67bd590346fd3c36
Russo-creation <69282925+Russo-creation@users.noreply.github.com>
2022-07-16 15:33:30 +0000

    Deploying to gh-pages from @ Russo-creation/my.portfolio@3fbd5f7fc10d9b6fee855e8ae5ef45b469eb7681 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my.portfolio/precache-manifest.dd0667789848f05fc581c5315a730a80.js"
+  "/my.portfolio/precache-manifest.31cf2c832df437c983e6bf2c09ccd8fb.js"
 );
 
 self.addEventListener('message', (event) => {
